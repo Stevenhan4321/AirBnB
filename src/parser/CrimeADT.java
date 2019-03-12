2396b14ecbcbf2acf94dabbfb451135cb42269ff
Steven <hanzy3@mcmaster.ca>
2019-03-12 15:54:59 -0400

    hey

---
+++ src/parser/CrimeADT.java
@@ -0,0 +1,86 @@
+package parser;
+
+public class CrimeADT {
+	
+	private String id;
+	private String offence;
+	private String residence;
+	private double longitude;
+	private double latitude;
+	
+	public CrimeADT(String id, String offence, String residence, double longitude, double latitude) {
+		this.id = id;
+		this.offence = offence;
+		this.residence = residence;
+		this.longitude = longitude;
+		this.latitude = latitude;
+	}
+	
+	public String getId() {
+		return this.id;
+	}
+	
+	public String getOffence() {
+		return this.offence;
+	}
+	
+	public String getResidence() {
+		return this.residence;
+	}
+	
+	public double getLongitude() {
+		return this.longitude;
+	}
+	
+	public double getLatitude() {
+		return this.latitude;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("Id: %s, Offence %s, Residence %s, Longitutde: %d, Latitude: %d", id, offence, residence, longitude, latitude);
+	}
+	
+	
+	
+	public int compareId(CrimeADT that) {
+		if (this.id.compareTo(that.getId()) < 0)
+			return -1;
+		if (this.id.compareTo(that.getId()) > 0)
+			return 1;
+		return 0;
+	}
+
+	
+	public int compareOffence(CrimeADT that) {
+		if (this.offence.compareTo(that.getOffence()) < 0)
+			return -1;
+		if (this.offence.compareTo(that.getOffence()) > 0)
+			return 1;
+		return 0;
+	}
+	
+	public int compareResidence(CrimeADT that) {
+		if (this.residence.compareTo(that.getResidence()) < 0)
+			return -1;
+		if (this.residence.compareTo(that.getResidence()) > 0)
+			return 1;
+		return 0;
+	}
+	
+	public int compareLongitude(CrimeADT that) {
+		if (this.longitude - (that.getLongitude()) < 0)
+			return -1;
+		if (this.longitude + (that.getLongitude()) > 0)
+			return 1;
+		return 0;
+	}
+	
+	public int compareLatitude(CrimeADT that) {
+		if (this.latitude - (that.getLatitude()) < 0)
+			return -1;
+		if (this.latitude + (that.getLatitude()) > 0)
+			return 1;
+		return 0;
+	}
+}
