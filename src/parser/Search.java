28e02023376074a92e3e3bb5459bd0095849ab1a
Steven <hanzy3@mcmaster.ca>
2019-03-12 22:07:47 -0400

    changed search

--- src/parser/Search.java
+++ src/parser/Search.java
@@ -1,34 +1,111 @@
 package parser;
 
 public class Search {
-	
-	public static int binarySearch(Comparable[] x, int lo, int hi, Comparable val){
-		if (lo == hi) {
-			return lo;
+
+	static int searchId(CrimeADT[] arr, String x) {
+		int l = 0, r = arr.length - 1;
+		while (l <= r) {
+			int m = l + (r - l) / 2;
+
+			int res = x.compareTo(arr[m].getId());
+			if (res == 0)
+				return m;
+			if (res > 0)
+				l = m + 1;
+			else
+				r = m - 1;
 		}
-		int mid = lo + ((hi - lo) / 2);
-		
-		if (val.compareTo(x[mid]) == 1) {
-			return binarySearch(x, mid+1, hi, val);
+		return -1;
+	}
+
+	static int searchOffence(CrimeADT[] arr, String x) {
+		int l = 0, r = arr.length - 1;
+		while (l <= r) {
+			int m = l + (r - l) / 2;
+
+			int res = x.compareTo(arr[m].getOffence());
+
+			// Check if x is present at mid
+			if (res == 0)
+				return m;
+			// If x greater, ignore left half
+			if (res > 0)
+				l = m + 1;
+			// If x is smaller, ignore right half
+			else
+				r = m - 1;
 		}
-		else if (val.compareTo(x[mid]) == -1) {
-			return binarySearch(x, lo, mid, val);
+		return -1;
+	}
+
+	static int searchResidence(CrimeADT[] arr, String x) {
+		int l = 0, r = arr.length - 1;
+		while (l <= r) {
+			int m = l + (r - l) / 2;
+
+			int res = x.compareTo(arr[m].getResidence());
+
+			// Check if x is present at mid
+			if (res == 0)
+				return m;
+			// If x greater, ignore left half
+			if (res > 0)
+				l = m + 1;
+			// If x is smaller, ignore right half
+			else
+				r = m - 1;
 		}
-		
-		return mid;
+		return -1;
+	}
+
+	static int searchLongitude(CrimeADT arr[], int l, int r, double d) {
+
+		if (r >= l) {
+			int mid = l + (r - l) / 2;
+
+			// If the element is present at the
+			// middle itself
+			if (arr[mid].getLongitude() == d)
+				return mid;
+
+			// If element is smaller than mid, then
+			// it can only be present in left subarray
+			if (arr[mid].getLongitude() > d)
+				return searchLongitude(arr, l, mid - 1, d);
+
+			// Else the element can only be present
+			// in right subarray
+			return searchLongitude(arr, mid + 1, r, d);
+		}
+
+		// We reach here when element is not present
+		// in array
+		return -1;
 	}
 	
-	public static int linearSearch(int arr[], int x) 
-	{ 
-	    int n = arr.length; 
-	    for(int i = 0; i < n; i++) 
-	    { 
-	        if(arr[i] == x) 
-	            return i; 
-	    } 
-	    return -1; 
-	} 
-	
-	
+	static int searchLatitude(CrimeADT arr[], int l, int r, double d) {
+
+		if (r >= l) {
+			int mid = l + (r - l) / 2;
+
+			// If the element is present at the
+			// middle itself
+			if (arr[mid].getLatitude() == d)
+				return mid;
+
+			// If element is smaller than mid, then
+			// it can only be present in left subarray
+			if (arr[mid].getLatitude() > d)
+				return searchLatitude(arr, l, mid - 1, d);
+
+			// Else the element can only be present
+			// in right subarray
+			return searchLatitude(arr, mid + 1, r, d);
+		}
+
+		// We reach here when element is not present
+		// in array
+		return -1;
+	}
 
 }
---
+++ src/parser/SearchTest.java
@@ -0,0 +1,62 @@
+package parser;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SearchTest {
+
+	CrimeADT crime1 = new CrimeADT("P1", "C1", "Residence", 1.32, 1.24);
+	CrimeADT crime2 = new CrimeADT("P2", "C2", "House", 2.32, 2.24);
+	CrimeADT crime3 = new CrimeADT("P3", "C3", "School", 6.32, 4.24);
+	CrimeADT crime4 = new CrimeADT("P4", "C4", "Mall", 8.32, 13.24);	
+	CrimeADT crime5 = new CrimeADT("P5", "C5", "School", 12.32, 14.24);	
+	CrimeADT crime6 = new CrimeADT("P6", "C6", "School", 23, 214.24);	
+	CrimeADT crime7 = new CrimeADT("P7", "C7", "Mall", 77.43, 644.24);	
+
+	CrimeADT[] test = {crime1, crime2, crime3, crime4, crime5, crime6, crime7};
+	
+	
+	@Before
+	public void setUp() throws Exception {
+
+	}
+
+
+	@Test
+	public void testSearchId() {
+		int i = Search.searchId(test, "P7");
+		assertTrue(i == 6);
+	}
+	
+	@Test
+	public void testSearchOffence() {
+		int i = Search.searchOffence(test, "C5");
+		assertTrue(i == 4);
+	}
+	
+	@Test
+	public void testSearchResidence() {
+		int i = Search.searchResidence(test, "Mall");
+		assertTrue(i == 3);
+	}
+
+	@Test
+	public void testSearchLongitude() {
+		int l = 0;
+		int r = test.length - 1;
+		int i = Search.searchLongitude(test, l, r, 8.32);
+		assertTrue(i == 3);
+	}
+	
+	@Test
+	public void testSearchLatitude() {
+		int l = 0;
+		int r = test.length - 1;
+		int i = Search.searchLatitude(test, l, r, 13.24);
+		assertTrue(i == 3);
+	}
+
+}
